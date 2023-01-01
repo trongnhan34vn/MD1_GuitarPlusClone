7dc3287de13411e92e1c1642e9c3ba0e82bb01ad
Nhan <nhan@iMac-Pro.local>
2023-01-01 20:44:28 +0700

    update

--- script/fixedBarCart.js
+++ script/fixedBarCart.js
@@ -1,9 +1,7 @@
 let checkCart = document.getElementById('check-cart');
 let checkPos = checkCart.getBoundingClientRect().height;
-console.log(checkPos)
 window.addEventListener("scroll", function (e) {
     let scrollPos = window.scrollY;
-    console.log(scrollPos,checkPos)
     if(scrollPos >= checkPos*2 ) {
         checkCart.style.transform = "translateY(100px)";
     } else {
